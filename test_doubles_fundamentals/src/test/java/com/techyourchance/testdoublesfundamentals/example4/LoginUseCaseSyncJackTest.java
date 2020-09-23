238f1a4f61ff7228f7c920dad179957263cba02f
Jack <gskwon@maxst.com>
2020-09-24 00:00:27 +0900

    Exer4_1. LoginUseCaseSyncTest - Define facke class ( LoginHttpEndpointSyncTd, AuthTokenCacheTd, EventBusPosterTd )

---
+++ test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/example4/LoginUseCaseSyncJackTest.java
@@ -0,0 +1,71 @@
+package com.techyourchance.testdoublesfundamentals.example4;
+
+import com.techyourchance.testdoublesfundamentals.example4.LoginUseCaseSync.UseCaseResult;
+import com.techyourchance.testdoublesfundamentals.example4.authtoken.AuthTokenCache;
+import com.techyourchance.testdoublesfundamentals.example4.eventbus.EventBusPoster;
+import com.techyourchance.testdoublesfundamentals.example4.eventbus.LoggedInEvent;
+import com.techyourchance.testdoublesfundamentals.example4.networking.LoginHttpEndpointSync;
+import com.techyourchance.testdoublesfundamentals.example4.networking.NetworkErrorException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class LoginUseCaseSyncJackTest {
+
+    LoginHttpEndpointSyncTd mLoginHttpEndpointSyncTd;
+    AuthTokenCacheTd mAuthTokenCacheTd;
+    EventBusPosterTd mEventBusPosterTd;
+
+    LoginUseCaseSync SUT;
+
+    @Before
+    public void setup() throws Exception {
+        mLoginHttpEndpointSyncTd = new LoginHttpEndpointSyncTd();
+        mAuthTokenCacheTd = new AuthTokenCacheTd();
+        mEventBusPosterTd = new EventBusPosterTd();
+        SUT = new LoginUseCaseSync(mLoginHttpEndpointSyncTd, mAuthTokenCacheTd, mEventBusPosterTd);
+    }
+
+    // User name 과 pwd 가 end point 에 전달된다.
+    // 로그인 성공하면 - auth token 이 cache 된다.
+    // 로그인 실패하면 - auth token 이 변하지 않는다.
+    // 로그인 성공하면 - login event 가 EventBusPoster 로 post 된다.
+    // 로그인 실패하면 - login event 가 EventBusPoster 에 post 되지 않는다.
+    // 로그인 성공하면 - Success return 된다.
+    // 로그인 실패하면 - Fail return 된다.
+    // 네트워크 에러면 - Network error 가 return 된다.
+
+    // ---------------------------------------------------------------------------------------------
+    // Helper classes
+
+    private static class LoginHttpEndpointSyncTd implements LoginHttpEndpointSync {
+
+        @Override
+        public EndpointResult loginSync(String username, String password) throws NetworkErrorException {
+            return null;
+        }
+    }
+
+    private static class AuthTokenCacheTd implements AuthTokenCache {
+        @Override
+        public void cacheAuthToken(String authToken) {
+
+        }
+
+        @Override
+        public String getAuthToken() {
+            return null;
+        }
+    }
+
+    private static class EventBusPosterTd implements EventBusPoster {
+        @Override
+        public void postEvent(Object event) {
+
+        }
+    }
+}
