1df36fe382212d5e95b20a47e369d9c7bb3529e1
Jack <gskwon@maxst.com>
2020-09-24 00:17:33 +0900

    Exer4_2._LoginHttpEndpointSyncTd -  Endpoint null pointer error

--- test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/example4/LoginUseCaseSyncJackTest.java
+++ test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/example4/LoginUseCaseSyncJackTest.java
@@ -16,56 +16,74 @@ import static org.junit.Assert.assertThat;
 
 public class LoginUseCaseSyncJackTest {
 
-    LoginHttpEndpointSyncTd mLoginHttpEndpointSyncTd;
-    AuthTokenCacheTd mAuthTokenCacheTd;
-    EventBusPosterTd mEventBusPosterTd;
+	public static final String USERNAME = "Username";
+	public static final String PASSWORD = "Password";
+	public static final String AUTH_TOKEN = "authToken";
 
-    LoginUseCaseSync SUT;
+	LoginHttpEndpointSyncTd mLoginHttpEndpointSyncTd;
+	AuthTokenCacheTd mAuthTokenCacheTd;
+	EventBusPosterTd mEventBusPosterTd;
 
-    @Before
-    public void setup() throws Exception {
-        mLoginHttpEndpointSyncTd = new LoginHttpEndpointSyncTd();
-        mAuthTokenCacheTd = new AuthTokenCacheTd();
-        mEventBusPosterTd = new EventBusPosterTd();
-        SUT = new LoginUseCaseSync(mLoginHttpEndpointSyncTd, mAuthTokenCacheTd, mEventBusPosterTd);
-    }
+	LoginUseCaseSync SUT;
 
-    // User name 과 pwd 가 end point 에 전달된다.
-    // 로그인 성공하면 - auth token 이 cache 된다.
-    // 로그인 실패하면 - auth token 이 변하지 않는다.
-    // 로그인 성공하면 - login event 가 EventBusPoster 로 post 된다.
-    // 로그인 실패하면 - login event 가 EventBusPoster 에 post 되지 않는다.
-    // 로그인 성공하면 - Success return 된다.
-    // 로그인 실패하면 - Fail return 된다.
-    // 네트워크 에러면 - Network error 가 return 된다.
+	@Before
+	public void setup() throws Exception {
+		mLoginHttpEndpointSyncTd = new LoginHttpEndpointSyncTd();
+		mAuthTokenCacheTd = new AuthTokenCacheTd();
+		mEventBusPosterTd = new EventBusPosterTd();
+		SUT = new LoginUseCaseSync(mLoginHttpEndpointSyncTd, mAuthTokenCacheTd, mEventBusPosterTd);
+	}
 
-    // ---------------------------------------------------------------------------------------------
-    // Helper classes
+	// User name 과 pwd 가 end point 에 전달된다.
+	@Test
+	public void loginSync_success_usernameAndPasswordPassedToEndpoint() throws Exception {
+		SUT.loginSync(USERNAME, PASSWORD);
+		assertThat(mLoginHttpEndpointSyncTd.mUsername, is(USERNAME));
+		assertThat(mLoginHttpEndpointSyncTd.mPassword, is(PASSWORD));
+	}
 
-    private static class LoginHttpEndpointSyncTd implements LoginHttpEndpointSync {
+	// 로그인 성공하면 - auth token 이 cache 된다.
+	// 로그인 실패하면 - auth token 이 변하지 않는다.
+	// 로그인 성공하면 - login event 가 EventBusPoster 로 post 된다.
+	// 로그인 실패하면 - login event 가 EventBusPoster 에 post 되지 않는다.
+	// 로그인 성공하면 - Success return 된다.
+	// 로그인 실패하면 - Fail return 된다.
+	// 네트워크 에러면 - Network error 가 return 된다.
 
-        @Override
-        public EndpointResult loginSync(String username, String password) throws NetworkErrorException {
+	// ---------------------------------------------------------------------------------------------
+	// Helper classes
+
+	private static class LoginHttpEndpointSyncTd implements LoginHttpEndpointSync {
+
+		public String mUsername;
+		public String mPassword;
+
+		@Override
+		public EndpointResult loginSync(String username, String password) throws NetworkErrorException {
+			mUsername = username;
+			mPassword = password;
+			// FIXME : Jack
             return null;
-        }
-    }
+			//return new LoginHttpEndpointSync.EndpointResult(EndpointResultStatus.GENERAL_ERROR, AUTH_TOKEN);
+		}
+	}
 
-    private static class AuthTokenCacheTd implements AuthTokenCache {
-        @Override
-        public void cacheAuthToken(String authToken) {
+	private static class AuthTokenCacheTd implements AuthTokenCache {
+		@Override
+		public void cacheAuthToken(String authToken) {
 
-        }
+		}
 
-        @Override
-        public String getAuthToken() {
-            return null;
-        }
-    }
+		@Override
+		public String getAuthToken() {
+			return null;
+		}
+	}
 
-    private static class EventBusPosterTd implements EventBusPoster {
-        @Override
-        public void postEvent(Object event) {
+	private static class EventBusPosterTd implements EventBusPoster {
+		@Override
+		public void postEvent(Object event) {
 
-        }
-    }
+		}
+	}
 }
