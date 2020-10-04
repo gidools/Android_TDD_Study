c14d2b6f0d198cc451a6a954bee7b1b3c6fd58c1
gidools <gskwon@maxst.com>
2020-10-04 12:15:34 +0900

    Exer7_1.TDD rules - Create FetchReputationUseCaseSync class and test class

---
+++ test_driven_development/src/main/java/com/techyourchance/testdrivendevelopment/exercise7/FetchReputationUseCaseSync.java
@@ -0,0 +1,4 @@
+package com.techyourchance.testdrivendevelopment.exercise7;
+
+public class FetchReputationUseCaseSync {
+}
---
+++ test_driven_development/src/test/java/com/techyourchance/testdrivendevelopment/exercise7/FetchReputationUseCaseSyncTest.java
@@ -0,0 +1,11 @@
+package com.techyourchance.testdrivendevelopment.exercise7;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FetchReputationUseCaseSyncTest {
+
+	private FetchReputationUseCaseSync fetchReputationUseCaseSync;
+}
