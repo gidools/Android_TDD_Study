6db769cad99ff2c1603d029c5f21b0e428171710
gidools <gskwon@maxst.com>
2020-10-07 23:30:44 +0900

    Exer3.IntervalsAdjacencyDetector - Create test class  // interval1 is before interval2  // interval1 is adjacent interval2 on start  // interval1 overlaps interval2 on start  // interval1 contains interval2  // interval1 is equal interval2  // interval1 is contained in interval2  // interval1 overlaps interval2 on end  // interval1 is adjacent interval2 on end  // interval1 is after interval2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
@@ -1,5 +1,16 @@
 package com.techyourchance.unittestingfundamentals.exercise3;
 
+import com.techyourchance.unittestingfundamentals.example3.Interval;
+
 public class IntervalsAdjacencyDetectorTest {
 
+	// interval1 is before interval2
+	// interval1 is adjacent interval2 on start
+	// interval1 overlaps interval2 on start
+	// interval1 contains interval2
+	// interval1 is equal interval2
+	// interval1 is contained in interval2
+	// interval1 overlaps interval2 on end
+	// interval1 is adjacent interval2 on end
+	// interval1 is after interval2
 }
